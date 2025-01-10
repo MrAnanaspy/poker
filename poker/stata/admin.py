291bce76935f0b925d196ebd460dcbcc4bc555b7
Stanislav <stassssssssssssss13@gmail.com>
2025-01-10 12:57:44 +0300

    add app poker

---
+++ poker/stata/admin.py
@@ -0,0 +1,75 @@
+from django.contrib import admin
+from django.db.models import Avg
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime
+from .models import Top, Person, Game
+
+
+# Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'score')
+    readonly_fields = ['score', 'date_create']
+    #list_filter = ('start_time', 'production_status', 'machine')
+
+    @admin.display(description="Person")
+    def name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".upper()
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'quantity', 'avg_score', 'comment')
+    readonly_fields = ['avg_score']
+    list_filter = ('date', 'quantity')
+
+
+@admin.register(Top)
+class TopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'person', 'game', 'place', 'price', 'score_gamer')
+    readonly_fields = ['score_gamer',]
+    list_filter = ('person', 'place', 'price')
+
+
+@receiver(post_save, sender=Top)
+def rating_distribution(sender, instance, created, **kwargs):
+    if created:
+        game = Game.objects.get(id=instance.game_id)
+        count = Top.objects.filter(game_id=game.id).count()
+        if game.quantity <= count:
+            for i in range(1, count+1):
+                avg_score = 0.0
+                for j in range(1, count+1):
+                    if i == j:
+                        continue
+                    avg_score = avg_score + Person.objects.get(id=(Top.objects.get(game_id=instance.game_id, place=j)).person_id).score
+                avg_score = avg_score/(count-1)
+                print(avg_score)
+                p = Person.objects.get(id=(Top.objects.get(game_id=instance.game_id, place=i)).person_id)
+                Ea = 1 / (1 + pow(10, (avg_score-p.score)/400))
+                print(Ea)
+                K = 20
+                if (Top.objects.filter(person_id=p.id).count() + 1) >=10:
+                    K = 40
+                elif p.score > 2400:
+                    K = 10
+                if i==count:
+                    Sa = 0
+                else:
+                    Sa = (1 / (count - 1)) * (count - i)
+                dop_score = round((K * (Sa - Ea)), 3)
+                new_score= round(p.score + dop_score, 3)
+                if new_score < 0:
+                    new_score = 0
+                print(f"{p.first_name} {p.last_name} изменился на: {dop_score}. {p.score} --> {new_score}")
+                p.score = new_score
+                p.save()
+                instance.score_gamer = new_score
+
+
+@receiver(post_save, sender=Person)
+def add_date(sender, instance, created, **kwargs):
+    if created:
+        if not instance.date_create:
+            instance.date_create = datetime.date.today()
---
+++ poker/stata/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+import datetime
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30, verbose_name='Имя')
+    last_name = models.CharField(max_length=30, null=True, blank=True, verbose_name='Фамилия')
+    score = models.FloatField(verbose_name='Рейтинг', default='100',)
+    date_create = models.DateField(verbose_name='Дата добавления', default=datetime.date.today())
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(verbose_name='Дата игры', default=datetime.date.today())
+    quantity = models.IntegerField(verbose_name='Количество игроков')
+    avg_score = models.FloatField(verbose_name='Средний счет за столом', default='100',)
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+
+    def __str__(self):
+        return f"№{self.id} - {self.date}"
+
+
+class Top(models.Model):
+    person = models.ForeignKey(Person, related_name='person', on_delete=models.CASCADE,
+                            max_length=10, verbose_name='Игрок')
+    game = models.ForeignKey(Game, related_name='game', on_delete=models.CASCADE,
+                               max_length=10, verbose_name='Игра')
+    place = models.IntegerField(verbose_name='Занятое место')
+    price = models.IntegerField(null=True, blank=True, verbose_name='Призовые')
+    score_gamer = models.FloatField(verbose_name='Очки игрока после игры', default='100',)
+
+    def __str__(self):
+        return f"{self.game}:{self.person}({self.place})"
