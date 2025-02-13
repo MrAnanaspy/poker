e38e849b3df3f8428626f9627b0356db84f5a19d
Stanislav <stassssssssssssss13@gmail.com>
2025-02-13 21:52:13 +0300

    линия изменения рейтинга

--- poker/stata/views.py
+++ poker/stata/views.py
@@ -4,7 +4,8 @@ from .models import Top, Person, Game
 
 
 def get_index(request):
-    data = Person.objects.all().order_by('-score').values()
-    for d in data:
-        print(d)
-    return render(request, "index.html", context={'data':data}) #, context={'exel':data}
+    persons = Person.objects.all().order_by('-score').values()
+    tops = Top.objects.all()
+    for i in tops:
+        print(i.game.date)
+    return render(request, "index.html", context={'persons':persons, 'tops':tops}) #, context={'exel':data}
