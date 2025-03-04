6203977d3bf30e518383076c72a37fd8c41e20fa
Stanislav <stassssssssssssss13@gmail.com>
2025-03-04 21:31:55 +0300

    Изменение айпи

--- poker/poker/settings.py
+++ poker/poker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a*n9oxu!qs8onp-&%(h(6ey#6#h*hvjzuhv(2!^-9i^@)r+e&k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.105', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.109', 'localhost', '127.0.0.1']
 
 
 # Application definition
